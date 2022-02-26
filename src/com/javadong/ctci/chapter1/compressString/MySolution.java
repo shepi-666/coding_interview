c0a486bfd9544aec83d7bb901907f37ced6c359f
javadong <dongshaowei1201@gmail.com>
2022-02-26 20:10:03 +0800

    [update]优化程序

--- src/com/javadong/ctci/chapter1/compressString/MySolution.java
+++ src/com/javadong/ctci/chapter1/compressString/MySolution.java
@@ -10,16 +10,13 @@ public class MySolution {
     public String compressString(String S) {
         if (S == null || S.length() < 1) return S;
         StringBuilder sb = new StringBuilder();
-        sb.append(S.charAt(0));
-        int count = 1;
-        for (int i = 1; i < S.length(); i++) {
-            while (i < S.length() && (S.charAt(i) == S.charAt(i - 1))) {
-                count++; i++;
+        int j = 0;
+        for (int i = 0; i < S.length(); ) {
+            while (j < S.length() && S.charAt(i) == S.charAt(j)) {
+                j ++ ;
             }
-            sb.append(count);
-            count = 1;
-            if (i < S.length()) sb.append(S.charAt(i));
-            if (i == S.length() - 1) sb.append(count);
+            sb.append(S.charAt(i)).append(j - i);
+            i = j;
         }
         return sb.toString().length() < S.length() ?
                 sb.toString() : S;
@@ -27,6 +24,6 @@ public class MySolution {
 
     @Test
     public void testSolution() {
-        System.out.println(compressString("lllhU"));
+        System.out.println(compressString("asaaajjjjjjjjhhhhhhhlllhU"));
     }
 }
